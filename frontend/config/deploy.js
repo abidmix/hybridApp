cb3708753ff61f07e880ecbd275812d7805e35df
Abide Masaraure <enduremasaraure@yahoo.com>
2015-07-15 15:19:58 -0400

    Update deploy.js

--- frontend/config/deploy.js
+++ frontend/config/deploy.js
@@ -18,8 +18,8 @@ module.exports = {
       "port": 6379
     },
     "assets": {
-      "accessKeyId": "AKIAI6GL2I3A2GBFBI3A",
-      "secretAccessKey": "+UJDPF8NPbJsnMyHxrKopmYo502ZVnEyPnxKLtCJ",
+      "accessKeyId": "<your-access-key-goes-here>",
+      "secretAccessKey": "<your-secret-access-key-goes-here>",
       "bucket": "abidmix",
       "prefix": "<optional-remote-prefix>",
       "gzipExtensions": ["js", "css", "svg", "json"]
